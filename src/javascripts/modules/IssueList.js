cd6c5b0ce9d957b890b708c75703aaf5480bbe9f
Eugene Dubinin <eugend@commandprompt.com>
2022-06-15 18:58:15 +0300

    implements colorization of grouped issues based on priority refs: #140820

--- src/javascripts/modules/IssueList.js
+++ src/javascripts/modules/IssueList.js
@@ -17,6 +17,25 @@ PurpleMine.IssueList = (function () {
     if ($('table.list.issues').length > 0) {
       if ($('th.parent > a.sort').length > 0) {
         $('table.list.issues').addClass('colorize')
+        var classMap = {
+          'priority-high3': 'colorize-yellow',
+          'priority-high2': 'colorize-orange',
+          'priority-highest': 'colorize-red'
+        }
+
+        var masterPrio
+        $('tr.issue').each(function () {
+          var row = $(this)
+          if (row.hasClass('parent') && !row.hasClass('child')) {
+            // priority-highest priority-high2 priority-high3
+            masterPrio = row.attr('class').match(/priority-[a-z]+\d*/)[0]
+            row.addClass(classMap[masterPrio])
+          }
+
+          if (row.hasClass('child')) {
+            row.addClass(classMap[masterPrio])
+          }
+        })
       }
     }
   }
